fcc1c9be5b56c5d1a73d7f3376cf22d4d8f3f07f
Camila Nachbar <mila.nachbar.@centauro.com.br>
2021-04-22 14:48:07 -0300

    Create Java.java

---
+++ src/main/java/br/com/magalu/aulas/reforço/Java.java
@@ -0,0 +1,19 @@
+package br.com.magalu.aulas.reforço;
+
+public class Java {
+    //O que é java?
+    //Java é uma linguagem de programação e plataforma computacional lançada pela primeira vez pela Sun Microsystems em 1995.
+    // Existem muitas aplicações e sites que não funcionarão, a menos que você tenha o Java instalado, e mais desses são criados todos os dias.
+    // O Java é rápido, seguro e confiável. De laptops a datacenters, consoles de games a supercomputadores científicos, telefones celulares à Internet, o Java está em todos os lugares!
+    //Por onde eu começo a estudar Java?
+    //Vamos dar início em "to-do" o conceito mas deixo material de suporte no final do ppt
+
+    //Java SE: Representa a base do Java, sendo composta pelas APIs e bibliotecas básicas para possibilitar o desenvolvimento de aplicativos de linha de comando e desktop. Já ouviu falar em Swing? Sim, ele faz parte do Java SE (Java Standard Edition);
+    //Java ME: A Java Micro Edition é a plataforma voltada para dispositivos móveis e embarcados, com capacidade de processamento reduzida, como os utilizados na criação de produtos para a Internet das Coisas;
+    //Java EE: Voltada para o desenvolvimento de soluções web e corporativas, a Java Enterprise Edition é uma especificação que agrupa outras, sendo, por isso, conhecida como especificação “guarda-chuva”.
+
+    //Na máquina virtual Java, ou JVM, é onde a sua aplicação será executada. É ela, também, a responsável pela característica multiplataforma do Java. Um programa escrito nessa linguagem será executado em qualquer plataforma, seja ela um notebook, smartphone ou torradeira, que possua uma máquina virtual Java implementada.
+
+    //Para quem não baixou e configurou a máquina:
+    //https://confluence.atlassian.com/confbr1/configurando-a-variavel-java_home-no-windows-933709538.html
+}
